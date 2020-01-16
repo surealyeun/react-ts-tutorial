dccf3c58bf1a32c30ca25634678a211b9b381745
surealyeun <monkyw@naver.com>
2020-01-16 18:28:52 +0900

    velopert의 리액트 프로젝트에서 타입스크립트 사용하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import Greetings from './Greetings';
+// import Counter from './Counter';
+// import MyForm from './MyForm';
+// import ReducerSample from './ReducerSample';
+import MyFormC from './MyForm copy';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // const onClick = (name: string) => {
+  //   console.log(name, 'says hello');
+  // }
+  // return <Greetings name="React" onClick={onClick} />;
+  
+  // return <Counter />;
+  
+  const onSubmit = (form: {name: string;description: string}) => {
+    console.log(form);
+  }
+  return <MyFormC onSubmit={onSubmit} />;
+
+  // return <ReducerSample />;
 }
 
 export default App;
